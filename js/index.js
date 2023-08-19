8b61d714c136535b08fc29bd22be325403a1097b
Oksana Ivashchenko <studoks08@gmail.com>
2023-08-19 19:19:53 +0300

    Double Char

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log(12);
+//1.Double Char
+//https://www.codewars.com/kata/56b1f01c247c01db92000076/fork/javascript
+function doubleChar(str) {
+  let outputString = '';
+
+  for (let i = 0; i < str.length; i++) {
+    outputString += str[i] + str[i];
+  }
+  return outputString;
+}
+
+console.log(doubleChar('abcd'));//aabbccdd
