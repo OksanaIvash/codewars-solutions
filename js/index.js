06750f4446e97d5b0dda045b1802a8a47ea29041
Oksana Ivashchenko <studoks08@gmail.com>
2023-08-19 20:12:37 +0300

    Convert a string to an array

--- js/index.js
+++ js/index.js
@@ -8,5 +8,19 @@ function doubleChar(str) {
   }
   return outputString;
 }
+console.log(doubleChar('abcd')); //aabbccdd
 
-console.log(doubleChar('abcd'));//aabbccdd
+//2.Convert a string to an array
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/fork/javascript
+const stringToArray = (string) => string.split(' ');
+const string = 'I love arrays they are my favorite';
+const strArray = stringToArray(string);
+console.log(strArray);
+
+//---------------------version2----------------
+// function stringToArray(string) {
+//   return string.split(' ');
+// }
+// const string = 'Robin Singh';
+// const strArray = stringToArray(string);
+// console.log(strArray);
