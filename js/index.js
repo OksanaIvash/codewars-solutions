90fe6c14b581d368a63ec82b52faf55aac00c7a3
Oksana Ivashchenko <studoks08@gmail.com>
2023-08-21 14:45:36 +0300

    Return n-th even number

--- js/index.js
+++ js/index.js
@@ -187,3 +187,8 @@ console.log(otherAngle(10, 20));
 const twiceAsOld = (dadYearsOld, sonYearsOld) =>
   Math.abs(dadYearsOld - sonYearsOld * 2);
 console.log(twiceAsOld(55, 30));
+
+//14.Return n-th even number
+//https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+const nthEven = (n) => n * 2 - 2;
+console.log(nthEven(1298734));
