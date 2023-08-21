605266af2522734e23f09e8012f20e46303c4858
Oksana Ivashchenko <studoks08@gmail.com>
2023-08-21 16:06:19 +0300

    Clock

--- js/index.js
+++ js/index.js
@@ -212,3 +212,8 @@ console.log(getRealFloor(15));
 //   if (n > 0) return n - 1;
 //   return n;
 // };
+
+//16.Clock
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+const past = (h, m, s) => (h * 3600 + m * 60 + s) * 1000;
+console.log(past(1, 0, 1));
