a0c18fc82de4e89b812e7f1c1326f927f38a4cd3
Oksana Ivashchenko <studoks08@gmail.com>
2023-09-14 13:51:44 +0300

    Get the Middle Character

--- js/index.js
+++ js/index.js
@@ -317,3 +317,12 @@ console.log(isIsogram('Dermatoglyphics'));
 
 const toNumberArray = (stringarray) => stringarray.map((num) => Number(num));
 console.log(toNumberArray(['1.1', '2.2', '3.3']));
+
+//24. Get the Middle Character
+//https://www.codewars.com/kata/56747fd5cb988479af000028/javascript
+
+function getMiddle(s) {
+  return s.length % 2 === 0
+    ? s.substr(s.length / 2 - 1, 2)
+    : s.substr((s.length - 1) / 2, 1);
+}
