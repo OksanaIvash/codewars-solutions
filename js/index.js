3da0387874427ffe9944b5ad28ecdcaa765d1314
Oksana Ivashchenko <studoks08@gmail.com>
2023-09-16 19:12:53 +0300

    String repeat

--- js/index.js
+++ js/index.js
@@ -361,3 +361,9 @@ function solve(n) {
 }
 
 console.log(solve(666));
+
+//27. String repeat
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+
+const repeatStr = (n, s) => s.repeat(n);
+console.log(repeatStr(2, 'ha '));
